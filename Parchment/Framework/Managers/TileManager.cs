67410fb3de9383c54ce8258e46b120d2a31d8cc1
Floogen <31755155+Floogen@users.noreply.github.com>
2026-07-23 22:26:19 -0500

    Adjusted TileManager to utilizing Tile.Properties

--- Parchment/Framework/Managers/TileManager.cs
+++ Parchment/Framework/Managers/TileManager.cs
@@ -10,12 +10,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using xTile.Dimensions;
+using xTile.ObjectModel;
 using xTile.Tiles;
 
 namespace Parchment.Framework.Managers
 {
     public class TileManager : BaseManager
     {
+        private const string BOOK_MARKER_PROPERTY = "ParchmentBookMarker";
+
         private Dictionary<TemporaryAnimatedSprite, string> _markersToQueries;
 
         public TileManager(IMonitor monitor, IModHelper helper) : base(monitor, helper)
@@ -55,49 +58,67 @@ namespace Parchment.Framework.Managers
 
         private void SpawnBookMarkers(GameLocation location)
         {
-            // Get tent tiles
-            var layer = location.Map.GetLayer("Buildings");
+            // Get any tiles flagged as a book marker
+            var layer = location.Map?.GetLayer("Buildings");
+            if (layer is null)
+            {
+                return;
+            }
 
             for (int x = 0; x < layer.LayerWidth; x++)
             {
                 for (int y = 0; y < layer.LayerHeight; y++)
                 {
-                    var bookMakerProperty = location.doesTileHaveProperty(x, y, "ParchmentBookMarker", "Buildings");
-                    if (string.IsNullOrEmpty(bookMakerProperty) is false)
+                    var tile = layer.Tiles[x, y];
+                    if (tile is null || TryGetBookMarkerProperty(tile, out string bookMarkerProperty) is false)
                     {
-                        string[] args = ArgUtility.SplitBySpaceQuoteAware(bookMakerProperty);
-                        if (ArgUtility.TryGetVector2(args, 0, out Vector2 offset, out string error, name: "offset") is false)
-                        {
-                            monitor.LogOnce($"The tile at ({x}, {y}) on the Buildings layer with the property \"ParchmentBookMarker\" has an invalid offset value. Must follow the format: X Y \"GSQ\"", LogLevel.Warn);
-                            continue;
-                        }
-                        if (ArgUtility.TryGetOptionalRemainder(args, 2, out string gameStateQuery) is false || string.IsNullOrWhiteSpace(gameStateQuery))
-                        {
-                            monitor.LogOnce($"The tile at ({x}, {y}) on the Buildings layer with the property \"ParchmentBookMarker\" has an invalid game state query. Must follow the format: X Y \"GSQ\"", LogLevel.Warn);
-                            continue;
-                        }
+                        continue;
+                    }
 
-                        if (GameStateQuery.CheckConditions(gameStateQuery))
-                        {
-                            var sprite = new TemporaryAnimatedSprite("LooseSprites\\Cursors", new Microsoft.Xna.Framework.Rectangle(144, 447, 15, 15), new Vector2((x * 64f) + offset.X, (y * 64f) + offset.Y - 96f - 16f), flipped: false, 0f, Color.White)
-                            {
-                                interval = 99999f,
-                                animationLength = 1,
-                                totalNumberOfLoops = 9999,
-                                yPeriodic = true,
-                                yPeriodicLoopTime = 4000f,
-                                yPeriodicRange = 16f,
-                                layerDepth = 1f,
-                                scale = 4f,
-                                id = (x + y) * 5000
-                            };
+                    string[] args = ArgUtility.SplitBySpaceQuoteAware(bookMarkerProperty);
+                    if (ArgUtility.TryGetVector2(args, 0, out Vector2 offset, out string error, name: "offset") is false)
+                    {
+                        monitor.LogOnce($"The tile at ({x}, {y}) on the Buildings layer with the property \"{BOOK_MARKER_PROPERTY}\" has an invalid offset value. Must follow the format: X Y \"GSQ\"", LogLevel.Warn);
+                        continue;
+                    }
+                    if (ArgUtility.TryGetOptionalRemainder(args, 2, out string gameStateQuery) is false || string.IsNullOrWhiteSpace(gameStateQuery))
+                    {
+                        monitor.LogOnce($"The tile at ({x}, {y}) on the Buildings layer with the property \"{BOOK_MARKER_PROPERTY}\" has an invalid game state query. Must follow the format: X Y \"GSQ\"", LogLevel.Warn);
+                        continue;
+                    }
 
-                            _markersToQueries.Add(sprite, gameStateQuery);
-                            location.temporarySprites.Add(sprite);
-                        }
+                    if (GameStateQuery.CheckConditions(gameStateQuery))
+                    {
+                        var sprite = new TemporaryAnimatedSprite("LooseSprites\\Cursors", new Microsoft.Xna.Framework.Rectangle(144, 447, 15, 15), new Vector2((x * 64f) + offset.X, (y * 64f) + offset.Y - 96f - 16f), flipped: false, 0f, Color.White)
+                        {
+                            interval = 99999f,
+                            animationLength = 1,
+                            totalNumberOfLoops = 9999,
+                            yPeriodic = true,
+                            yPeriodicLoopTime = 4000f,
+                            yPeriodicRange = 16f,
+                            layerDepth = 1f,
+                            scale = 4f,
+                            id = ((y * layer.LayerWidth) + x) * 5000
+                        };
+
+                        _markersToQueries.Add(sprite, gameStateQuery);
+                        location.temporarySprites.Add(sprite);
                     }
                 }
             }
+        }
+
+        private static bool TryGetBookMarkerProperty(Tile tile, out string value)
+        {
+            if (tile.Properties.TryGetValue(BOOK_MARKER_PROPERTY, out PropertyValue? property) is false && tile.TileIndexProperties.TryGetValue(BOOK_MARKER_PROPERTY, out property) is false)
+            {
+                value = string.Empty;
+                return false;
+            }
+
+            value = property?.ToString() ?? string.Empty;
+            return string.IsNullOrEmpty(value) is false;
         }
     }
 }
